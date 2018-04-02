9756409e615ec64c9fc6c58550747abf821e4432
mackenna95 <hillmj@umich.edu>
2018-04-02 17:38:14 -0400

    Finalizes function

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import axios from 'axios'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Button from 'material-ui/Button';
 import TextField from 'material-ui/TextField';
-import ActionFavoriteBorder from 'material-ui/svg-icons/action/favorite-border';
-import AutoComplete from 'material-ui/AutoComplete';
 
 // want to use gunicorn (rather than flask) deployed on the vcm
 // mongoDB
@@ -13,7 +11,7 @@ import AutoComplete from 'material-ui/AutoComplete';
 // eslint-disable-next-line
 const style = {
         "margin": "20px",
-        "color": "blue",
+        "color": "black",
 }
 
 class App extends React.Component {
@@ -28,12 +26,10 @@ class App extends React.Component {
 
 // http://vcm-3587.vm.duke.edu:5000/api/heart_rate/suyash@suyashkumar.com
     getData = () => {
-        //userInput = TextField.getValue()
-        this.setState({inputValue: TextField.getValue});
-        axios.get("http://adpl.suyash.io/api/sites").then( (response) => {
+        axios.get("http://vcm-3587.vm.duke.edu:5000/api/heart_rate/" + this.state.inputValue).then( (response) => {
             console.log(response);
             console.log(response.status); // this is equivalent to response["status"]
-            this.setState({"data": response.data});
+            this.setState(response.data);
         });
     }
     
@@ -42,18 +38,14 @@ class App extends React.Component {
       inputValue: evt.target.value
     });
   }
-    
-// need to get this grabbing values from the database
-  handleUpdateInput = (value) => {
-    this.setState({
-      dataSource: [
-        "Derrick",
-        value + value,
-        value + value + value,
-      ],
-    });
-  };
 
+// could create a new array that is a tuple of the two values and then print that
+// new = hr.map(e) => {
+    // return(
+        //<raw>
+            //e
+        //</raw>
+    //}),
   render() {
     return (
       <div className="App">
@@ -61,25 +53,23 @@ class App extends React.Component {
           <h1 className="App-title">Basic Heart Rate Viewer</h1>
         </header>
         <p className="App-intro">
-          To get started...
         </p>
           <div>
             <MuiThemeProvider>
-                <AutoComplete
-                  hintText="suyash@suyashkumar.com"
-                  dataSource={this.state.dataSource}
-                  filter={AutoComplete.caseInsensitiveFilter}
-                  //onUpdateInput={this.handleUpdateInput}
-                  onChange={evt => this.updateInputValue(evt)}
-                  floatingLabelText="Enter Patient Email"
-                  floatingLabelFixed={true}
+                <TextField
+                    id="with-placeholder"
+                    label="Enter Patient Email"
+                    placeholder="suyash@suyashkumar.com"
+                    className={this.props.textField}
+                    margin="normal"
+                    onChange={evt => this.updateInputValue(evt)}
                 /><br />
-                <Button variant="raised" label="Display Patient Heart Rate" primary={true} style={style} onClick={this.getData}>
-                    <ActionFavoriteBorder />
+                <Button variant="raised" color="secondary" style={style} onClick={this.getData}>
+                    Display Patient Heart Rate
                 </Button>
             </MuiThemeProvider>
-            {this.state.data[0]}
-            {this.state.inputValue}
+            {this.state.date_time}
+            {this.state.heart_rate}
           </div>
       </div>
     );
